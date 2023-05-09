f6b192142422e54b242b45cd991efb2b850e264a
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-05-09 15:06:44 +0200

    express quest 04 challenge

--- app.js
+++ app.js
@@ -24,9 +24,10 @@ app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
 
 
-
 app.post("/api/movies", movieHandlers.postMovie)
 app.post("/api/users", usersHandlers.postUser)
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", usersHandlers.updateUser)
 
 app.listen(port, (err) => {
   if (err) {
