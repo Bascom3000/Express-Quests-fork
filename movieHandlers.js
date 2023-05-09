e3641ee90ed2df8f59852bec799f773bb2eb9781
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-05-09 12:19:30 +0200

    express quest 03 challenge

--- movieHandlers.js
+++ movieHandlers.js
@@ -28,36 +28,6 @@
 const database = require("./database");
 
 
-const getUsers = (req, res) => {
-  database
-  .query("select * from users")
-  .then(([users]) => {
-    res.json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error retrieving data from database");
-  })
-};
-
-const getUsersById =(req, res) => {
-  const id = parseInt(req.params.id)
-
-  database
-  .query("select * from users where id = ?", [id])
-  .then(([users]) => {
-    if (users[0] != null) {
-      res.json(users[0]);
-    }else{
-      res.status(404).send("Not Found")
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(404).send("Not Found");
-  });
-};
-
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
@@ -89,9 +59,25 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUsersById
+  postMovie
 };
