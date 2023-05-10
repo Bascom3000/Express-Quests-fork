e454636063ac1fb2903526543807bca802dd837c
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-05-10 14:41:49 +0200

    express quest 04 bis challenge done

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
-const Joi = require("joi");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -17,68 +16,14 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-const usersHandlers = require("./usersHandlers")
-
-// validation for post & put
-const validateMovie = (req, res, next) => {
-  const { title, director, year, color, duration } = req.body;
-  const errors = [];
-
-  if (title == null) {
-    errors.push({ field: "title", message: "This field is required" });
-  } else if (title.length >= 255) {
-    errors.push({ field: "title", message: "Should contain less than 255 characters" });
-  }
-  if (director == null) {
-    errors.push({ field: "director", message: "This field is required" });
-  }
-  if (year == null) {
-    errors.push({ field: "year", message: "This field is required" });
-  }
-  if (color == null) {
-    errors.push({ field: "color", message: "This field is required" });
-  }
-  if (duration == null) {
-    errors.push({ field: "duration", message: "This field is required" });
-  }
- 
-  if (errors.length) {
-    res.status(422).json({ validationErrors: errors });
-  } else {
-    next();
-  }
-};
-
-const userSchema = Joi.object({
-  email: Joi.string().email().max(255).required(),
-  firstname: Joi.string().max(255).required(),
-  lastname: Joi.string().max(255).required(),
-  city: Joi.string().max(255).required(),
-  language: Joi.string().max(255).required(),
-});
-
-const validateUser = (req, res, next) => {
-  const { firstname, lastname, email, city, language } = req.body;
-  const { error } = userSchema.validate(
-    { firstname, lastname, email, city, language },
-    { abortEarly: false }
-  );
-
-  if (error) {
-    res.status(422).json({ validationErrors: error.details });
-  } else {
-    next();
-  }
-}
+const usersHandlers = require("./usersHandlers");
+const validators = require("./validators");
 
 
-
-
-
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.post("/api/users", validateUser, usersHandlers.postUser);
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-app.put("/api/users/:id", validateUser, usersHandlers.updateUser);
+app.post("/api/movies", validators.validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validators.validateUser, usersHandlers.postUser);
+app.put("/api/movies/:id", validators.validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validators.validateUser, usersHandlers.updateUser);
 
 
 app.get("/api/movies", movieHandlers.getMovies);
