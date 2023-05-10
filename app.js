0433eed4b64c03adc149b42b1bb9afd507483023
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-05-10 14:29:58 +0200

    express quest 04 bis challenge

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+const Joi = require("joi");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -18,18 +19,77 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const usersHandlers = require("./usersHandlers")
 
+// validation for post & put
+const validateMovie = (req, res, next) => {
+  const { title, director, year, color, duration } = req.body;
+  const errors = [];
+
+  if (title == null) {
+    errors.push({ field: "title", message: "This field is required" });
+  } else if (title.length >= 255) {
+    errors.push({ field: "title", message: "Should contain less than 255 characters" });
+  }
+  if (director == null) {
+    errors.push({ field: "director", message: "This field is required" });
+  }
+  if (year == null) {
+    errors.push({ field: "year", message: "This field is required" });
+  }
+  if (color == null) {
+    errors.push({ field: "color", message: "This field is required" });
+  }
+  if (duration == null) {
+    errors.push({ field: "duration", message: "This field is required" });
+  }
+ 
+  if (errors.length) {
+    res.status(422).json({ validationErrors: errors });
+  } else {
+    next();
+  }
+};
+
+const userSchema = Joi.object({
+  email: Joi.string().email().max(255).required(),
+  firstname: Joi.string().max(255).required(),
+  lastname: Joi.string().max(255).required(),
+  city: Joi.string().max(255).required(),
+  language: Joi.string().max(255).required(),
+});
+
+const validateUser = (req, res, next) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  const { error } = userSchema.validate(
+    { firstname, lastname, email, city, language },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+}
+
+
+
+
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, usersHandlers.postUser);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, usersHandlers.updateUser);
+
+
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users/", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-
-
-app.post("/api/movies", movieHandlers.postMovie)
-app.post("/api/users", usersHandlers.postUser)
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", usersHandlers.updateUser)
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", usersHandlers.deleteUser);
+
 
 
 app.listen(port, (err) => {
---
+++ movieOLD.js
@@ -0,0 +1,11 @@
+const getMovies = (req, res) => {
+    database
+      .query("select * from movies")
+      .then(([movies]) => {
+        res.json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
